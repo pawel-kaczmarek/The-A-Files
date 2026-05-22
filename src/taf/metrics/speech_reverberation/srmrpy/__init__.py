3a457379aa094dbb90e38bce8738799280a289d0
Paweł Kaczmarek <pawelkaczmarek12@gmail.com>
2026-05-22 23:38:08 +0200

    The A-Files - add: publishing

---
+++ src/taf/metrics/speech_reverberation/srmrpy/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# Copyright 2014 Joao Felipe Santos, jfsantos@emt.inrs.ca
+#
+# This file is part of the SRMRpy library, and is licensed under the
+# MIT license. See LICENSES/SRMRpy-LICENSE.md in this distribution.
+
+from srmrpy.srmr import srmr
+
+__all__ = ["srmr"]
