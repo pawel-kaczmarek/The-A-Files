dca90c09c5996b79b5e2e4541d85df7701d3ea7b
Paweł Kaczmarek <pawelkaczmarek12@gmail.com>
2026-07-05 22:59:57 +0100

    The A-Files - version 1.0.0

--- tests/test_package_import.py
+++ tests/test_package_import.py
@@ -12,7 +12,7 @@ from taf.models.types import MethodType, MetricType
 class PackageImportTests(unittest.TestCase):
     def test_taf_import(self) -> None:
         self.assertTrue(taf)
-        self.assertEqual(version("the-a-files"), "0.1.1")
+        self.assertEqual(version("the-a-files"), "1.0.0")
 
     def test_core_enum_imports(self) -> None:
         self.assertTrue(MethodType)
